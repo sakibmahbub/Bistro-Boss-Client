7ff7e147c27b01d8814d71ff2df239efd4189bcf
sakibmahbub <arafatsakib48@gmail.com>
2023-05-25 13:34:57 +0600

    added dynamic section title

--- src/Components/SectionTitle/SectionTitle.jsx
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-
-const SectionTitle = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const SectionTitle = ({ heading, subHeading }) => {
+  return (
+    <div className="mx-auto md:w-4/12 text-center mb-12">
+      <p className="text-[#D99904] text-lg mb-3">---{subHeading}---</p>
+      <h2 className="text-5xl uppercase border-y-4 p-4">{heading}</h2>
+    </div>
+  );
 };
 
 export default SectionTitle;
--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -8,10 +8,15 @@ import slide2 from "../../../assets/home/slide2.jpg";
 import slide3 from "../../../assets/home/slide3.jpg";
 import slide4 from "../../../assets/home/slide4.jpg";
 import slide5 from "../../../assets/home/slide5.jpg";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
 const Category = () => {
   return (
-    <div>
+    <section className="my-24">
+      <SectionTitle
+        subHeading={"From 11.00 am to 10.00 pm"}
+        heading={"Order Online"}
+      ></SectionTitle>
       <Swiper
         slidesPerView={4}
         spaceBetween={30}
@@ -20,7 +25,7 @@ const Category = () => {
           clickable: true,
         }}
         modules={[Pagination]}
-        className="mySwiper mb-20"
+        className="mySwiper"
       >
         <SwiperSlide>
           <div className="relative">
@@ -68,7 +73,7 @@ const Category = () => {
           </div>
         </SwiperSlide>
       </Swiper>
-    </div>
+    </section>
   );
 };
 

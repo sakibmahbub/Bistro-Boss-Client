df5bcff83e7b2e865116629888b5c1686a92939c
sakibmahbub <arafatsakib48@gmail.com>
2023-05-27 14:08:03 +0600

    displayed food items on different tabs

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+const FoodCard = ({ item }) => {
+  const { name, image, price, recipe } = item;
+  return (
+    <div>
+      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img src={image} alt={name} />
+        </figure>
+        <p className="bg-slate-900 absolute right-0 mr-4 mt-4 p-1 text-white">
+          ${price}
+        </p>
 
-const FoodCard = () => {
-  return <div></div>;
+        <div className="card-body text-center">
+          <h2 className="card-title flex justify-center">{name}</h2>
+          <p>{recipe}</p>
+          <div className="card-actions justify-center mt-3">
+            <button className="btn btn-sm bg-slate-900">Add To Cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FoodCard;
--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -4,6 +4,7 @@ import Cover from "../../Shared/Cover/Cover";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import useMenu from "../../../hooks/useMenu";
+import OrderTab from "../OrderTab/OrderTab";
 
 const Order = () => {
   const [tabIndex, setTabIndex] = useState(0);
@@ -12,23 +13,33 @@ const Order = () => {
   const soup = menu.filter((item) => item.category === "soup");
   const salad = menu.filter((item) => item.category === "salad");
   const pizza = menu.filter((item) => item.category === "pizza");
-  const offered = menu.filter((item) => item.category === "offered");
+  const drinks = menu.filter((item) => item.category === "drinks");
   return (
     <div>
       <Cover img={orderCover} title="Order Food"></Cover>
-      <Tabs defaultIndex={1} onSelect={(index) => setTabIndex(index)}>
+      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
         <TabList>
           <Tab>Salad</Tab>
           <Tab>Pizza</Tab>
           <Tab>Soup</Tab>
           <Tab>Dessert</Tab>
-          <Tab>Drink</Tab>
+          <Tab>Drinks</Tab>
         </TabList>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
+        <TabPanel>
+          <OrderTab items={salad}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={pizza}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={soup}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={dessert}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={drinks}></OrderTab>
+        </TabPanel>
       </Tabs>
     </div>
   );

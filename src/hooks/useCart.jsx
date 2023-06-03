912355a9c3ed1fadb57b5988fd66c260a7e0ff95
sakibmahbub <arafatsakib48@gmail.com>
2023-06-03 14:06:17 +0600

    send jwt token to backend for verification

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -3,12 +3,18 @@ import { AuthContext } from "../providers/AuthProvider";
 import { useContext } from "react";
 const useCart = () => {
   const { user } = useContext(AuthContext);
+  const token = localStorage.getItem("access-token");
 
   const { refetch, data: cart = [] } = useQuery({
     queryKey: ["carts", user?.email],
     queryFn: async () => {
       const res = await fetch(
-        `http://localhost:5000/carts?email=${user.email}`
+        `http://localhost:5000/carts?email=${user.email}`,
+        {
+          headers: {
+            authorization: `bearer ${token}`,
+          },
+        }
       );
       return res.json();
     },
